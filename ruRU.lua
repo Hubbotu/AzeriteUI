dc17b050aba643544d345b14f5a521ee43da190f
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-04-24 08:03:17 +0300

    Add files via upload

---
+++ ruRU.lua
@@ -0,0 +1,5 @@
+--Eq set bug fixes
+T["Swapback item not found in bags, cannot re-equip."] = "Swapback item not found in bags, cannot re-equip."
+T["A bug with the Angleur Set has occurred, where it is set to unequip all gear. " 
+.. "Therefore, it has been deleted. If this keeps happening, please contact the Author."] = "A bug with the Angleur Set has occurred, where it is set to unequip all gear. " 
+.. "Therefore, it has been deleted. If this keeps happening, please contact the Author."
